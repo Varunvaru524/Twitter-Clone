ee161c36f59eb1716e6b4e14cb159c757517b908
Varun <varunvaru524@gmail.com>
2023-03-17 10:46:51 +0530

    Addeed login, signup and notfound pages

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/Login'
+import SignupPage from './pages/Signup'
+import NotfoundPage from './pages/Notfound'
+
+
+class App extends Component {
+    render() {
+        return (
+            <Routes>
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/signup' element={<SignupPage />} />
+                <Route path='*' element={<NotfoundPage />} />
+            </Routes>
+        );
+    }
+}
+
+export default App;
