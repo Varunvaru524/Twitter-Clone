c166ba63c27c36fb6e189adf4917be057a8541db
Varun <varunvaru524@gmail.com>
2023-03-19 02:20:03 +0530

    Addedsearch box to home page

--- src/Experimental.jsx
+++ src/Experimental.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import TweetButton from './Components/Buttons/MainButton/MainButton'
+import TextBox from './Components/FormElements/TextBox/TextBox'
 
 class Experimental extends Component {
     render() {
         return (
             <>
-            <TweetButton  inputText='Tweet' className="tweetButton"/>
-            <TweetButton  className="primaryButton"/>
-            <TweetButton  className="secondaryButton"/>
+            <TextBox/>
             </>
         );
     }
--- src/pages/homePage/Components/RightSideBar/RightSideBar.jsx
+++ src/pages/homePage/Components/RightSideBar/RightSideBar.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import TextBox from '../../../../Components/FormElements/TextBox/TextBox';
 import './RightSideBar.css'
 
 class RightSideBar extends Component {
     render() { 
         return (
             <div className='rightSidebarContainer'>
-                <div>Search</div>
+                <TextBox placeHolder='Search' />
                 <div>Wjats Happening</div>
                 <div>Who to follow</div>
             </div>
