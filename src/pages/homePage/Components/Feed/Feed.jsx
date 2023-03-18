79cc54f1c07888cc13e1f0f654609f4752eda4fd
Varun <varunvaru524@gmail.com>
2023-03-19 01:31:07 +0530

    Added some css for home page

--- src/pages/homePage/Components/Feed/Feed.jsx
+++ src/pages/homePage/Components/Feed/Feed.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import './Feed.css'
 
 class Feed extends Component {
     render() { 
-        return (<div>Feed</div>);
+        return (<div className='feedContainer'>Feed</div>);
     }
 }
  
--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import LeftSidebar from './Components/LeftSideBar/LeftSideBar'
 import RightSidebar from './Components/RightSideBar/RightSideBar'
 import Feed from './Components/Feed/Feed'
+import './homePage.css'
 
 class HomePage extends Component {
     render() {
         return (
-            <main>
-                <LeftSidebar />
-                <Feed />
-                <RightSidebar />
+            <main className='homePage'>
+                <div className='homePageLeftSidebar'>
+                    <LeftSidebar />
+                </div>
+                <div className="homePageFeed">
+                    <Feed />
+                </div>
+                <div className='homePageRightSidebar'>
+                    <RightSidebar />
+                </div>
             </main>
         );
     }
