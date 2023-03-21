df2063429c0a9cc1516a6eb0cb9879937dbee329
Varun <varunvaru524@gmail.com>
2023-03-22 00:59:50 +0530

    Updated Higher order component of router hooks

--- src/Components/HigherOrderComponents/RouterHooks.jsx
+++ src/Components/HigherOrderComponents/RouterHooks.jsx
@@ -1,18 +1,25 @@
-import {useNavigate,useLocation,useParams,useSearchParams} from 'react-router-dom'
+import { useNavigate, useLocation, useParams, useSearchParams, } from "react-router-dom";
 
-const RouterHooks = ({Component}) => {
-    const navigate = useNavigate()
-    const location = useLocation()
-    const params = useParams()
-    return ( 
-        <Component navigate={navigate} location={location} params={params} />
-     );
-}
+const RouterHooks = ({ Component }) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const params = useParams();
+    const [searchParams, setSearchParams] = useSearchParams();
+    
+    return (
+        <Component
+            navigate={navigate}
+            location={location}
+            params={params}
+            searchParams={searchParams}
+            setSearchParams={setSearchParams}
+        />
+    );
+};
 
 RouterHooks.defaultProps = {
     // Must send a React Component
-    Component:null
-}
- 
-export default RouterHooks;
+    Component: null,
+};
 
+export default RouterHooks;
